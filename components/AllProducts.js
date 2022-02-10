09e42aac39d1b5dbef9ac71c7f689fa51cd4ac00
izhar360 <muhammadizhar360@gmail.com>
2022-02-10 09:13:55 +0500

    card adjusted

--- components/AllProducts.js
+++ components/AllProducts.js
@@ -15,7 +15,7 @@ const AllProducts = ({ products, name }) => {
       </div>
 
       <div className="flex  justify-center content-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-12 justify-content-center">
+        <div className="px-5 grid grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-5 md:gap-12 justify-content-center">
           {products?.map((item, i) => (
             <div
               key={i}
@@ -25,16 +25,16 @@ const AllProducts = ({ products, name }) => {
                 className="rounded-t-lg object-cover"
                 src={"https:" + item.fields.image.fields.file.url}
                 alt="product image"
-                width={280}
-                height={240}
+                width={320}
+                height={280}
               />
               <Link href={"/product/" + item.fields.slug}>
-                <div className="px-5 pt-4 pb-5">
-                  <h3 className="text-lg  pb-4 font-semibold tracking-tight text-gray-900 ">
+                <div className="px-5 pt-3 pb-3 md:pt-4 md:pb-5">
+                  <h3 className="text-lg pb-2  md:pb-4 font-semibold tracking-tight text-gray-900 ">
                     {item.fields.Name}
                   </h3>
 
-                  <p className="text-white cursor-pointer  focus:ring-4  font-medium rounded-lg text-sm px-4 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                  <p className="text-white cursor-pointer  focus:ring-4  font-medium rounded-lg text-xs md:text-sm px-2 md:px-4 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                     Order on Whatsapp
                   </p>
                 </div>
